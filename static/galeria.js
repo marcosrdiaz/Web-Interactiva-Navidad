99b6aa6c0b670e58aaa676af7b34125e65fd3054
100495901 <100495901@alumnos.uc3m.es>
2024-12-11 11:28:35 +0100

    CARTASSSSS

--- static/galeria.js
+++ static/galeria.js
@@ -28,5 +28,5 @@ function showSlides(n) {
   }
 
   slides[slideIndex - 1].style.display = "block";  
-  dots[slideIndex - 1].className += " active";
+  //dots[slideIndex - 1].className += " active";
 }
